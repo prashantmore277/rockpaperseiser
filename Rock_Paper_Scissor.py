5d9b6dabd9067fb4ca09b892cc11e161a2b9e28e
Dernyt-TPE <pranjalsr.biz@gmail.com>
2020-10-26 01:19:11 +0530

    Modified Code

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,16 +1,17 @@
 import random as rdm
+import sys
 
 #Define game function
 
 def game():
-	print("Welcome to Rock Paper Sizzer\n")
+	print("Welcome to Rock Paper Scissor\n")
 	
 	print("INFO :-")
 	Name = str(input("Your Name : "))
 	City = str(input("Your City : "))
 	
 	print("\nGame Rules :")
-	print("1 ] R=Rock P=Paper S=Sizzer\n2 ] Rock blends Sizzer\n3 ] Paper covers Rock\n4 ] Sizzer cuts Paper\n5 ] If Player won 1 point\n    added in players account\n6 ] If Bot won 1 point goes\n    in bots account\n7 ] For Tie one point add\n    in both players account\n8 ] Total matches compalsory\n9 ] At end Winner will be annouced\n")
+	print("1 ] R=Rock P=Paper S=Scissor\n2 ] Rock blends Scissor\n3 ] Paper covers Rock\n4 ] Scissor cuts Paper\n5 ] If Player won 1 point\n    added in players account\n6 ] If Bot won 1 point goes\n    in bots account\n7 ] For Tie one point add\n    in both players account\n8 ] Total matches compalsory\n9 ] At end Winner will be annouced\n")
 	
 	Count= 10
 	Your_score=0
@@ -23,26 +24,26 @@ def game():
 		print("~~~~~~~~~~~~~~~~~~~~~~~\n")
 		Count-=1
 		print("Round No = ",10-Count)
-		print("\nYour Choise :\n")
-		print("Rock = 1\nPaper = 2\nSizzer = 3\n")
-		List2 = ["Rock","Paper","Sizzer"]
+		print("\nYour choice :\n")
+		print("Rock = 1\nPaper = 2\nScissor = 3\n")
+		List2 = ["Rock","Paper","Scissor"]
 		P= int(input("Choose 1/2/3 = " ))
 		B =rdm.randint(1,3)
 		
 		if(P==B):
 			
-			print("\nYour choise :",List2[P-1])
-			print("Bot choise :",List2[B-1],"\n")
+			print("\nYour choice :",List2[P-1])
+			print("Bot choice :",List2[B-1],"\n")
 			print("__________Tie__________\n")
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
 			
 			
 		elif(P==1 and B!=1 and B!=2 and B==3):
-			print("Your choise :",List2[P-1])
-			print("Bot choise :",List2[B-1])
-			print("____Winner Winner____\n")
-			print("\nRock blends Sizzer\n")
+			print("Your choice :",List2[P-1])
+			print("Bot choice :",List2[B-1])
+			print("\n____Winner Winner____")
+			print("\nRock blends Scissor\n")
 			Your_score += 1
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
@@ -50,11 +51,11 @@ def game():
 			
 			
 		elif (P==2 and B!=2 and B!=3 and B==1):
-			print("Your choise :",List2[P-1])
-			print("Bot choise :",List2[B-1])
+			print("Your choice :",List2[P-1])
+			print("Bot choice :",List2[B-1])
 			print("\n")
 			print("  🤠🤠🤠🤠🤠🤠🤠🤠")
-			print("____Winner Winner____\n")
+			print("____Winner Winner____")
 			print("\nPaper covers Rock\n")
 			Your_score += 1
 			print("Your Score : ",Your_score)
@@ -62,25 +63,25 @@ def game():
 			
 			
 		elif(P==3 and B!=3 and B!=1 and B==2):
-			print("Your choise :",List2[P-1])
-			print("Bot choise :",List2[B-1])
+			print("Your choice :",List2[P-1])
+			print("Bot choice :",List2[B-1])
 			print("\n")
-			print("____Winner Winner____\n")
-			print("\nSizzer cuts Paper\n")
+			print("____Winner Winner____")
+			print("\nScissor cuts Paper\n")
 			Your_score += 1
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
 						
 		elif( P!=B ):
-			print("Your choise :",List2[P-1])
-			print("Bot choise :",List2[B-1])
+			print("Your choice :",List2[P-1])
+			print("Bot choice :",List2[B-1])
 			print("\n")
 			print("____Ping Pong Looser____\n")
 			Bot_score += 1
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
 		
-		print("Match Remaning :",Count,"\n")
+		print("Match Remaining :",Count,"\n")
 		print("~~~~~~~~~~~~~~~~~~~~~~~\n")
 		print("\n")		
 	print("\n")
@@ -90,7 +91,7 @@ def game():
 			print("~~~~~~~~~~~~~~~~~~~~~~\n")
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
-			status = "Winner"
+			status = "Congratulations - You WON !!"
 			print ("😃😃",status,"😀😀")
 			print("~~~~~~~~~~~~~~~~~~~~~~\n")
 			#print("~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -99,7 +100,7 @@ def game():
 			print("~~~~~~~~~~~~~~~~~~~~~~\n")
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
-			status = "Looser"
+			status = "Hard Luck - You Lost !!"
 			print ("😡😡",status,"😡😡")
 			print("~~~~~~~~~~~~~~~~~~~~~~\n")
 			#print("~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -108,7 +109,7 @@ def game():
 			print("~~~~~~~~~~~~~~~~~~~~~~\n")
 			print("Your Score : ",Your_score)
 			print("Bot Score : ",Bot_score,"\n")
-			status = "Tied"
+			status = "Oops Game Tied"
 			print ("😑😑",status,"😑😑")
 			
 			#print("~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -145,11 +146,11 @@ def clear_records():
 	file = open("pygamerecord.txt","r+")
 	file. truncate()
 	file. close()
-	print("\nRecords Clear Succesfully.....\n")
+	print("\nRecords Clear Successfully.....\n")
 	menu()
 	
 def menu_dup_record():
-		print("\nDuplecate Records: \n1 ] Type 1 to Create  ==> Winner Entry\n2 ] Type 2 to Create ==> Looser Entry\n3 ] Type 3 to Create ==> Tied Entry\n=>")
+		print("\nDuplicate Records: \n1 ] Type 1 to Create  ==> Winner Entry\n2 ] Type 2 to Create ==> Looser Entry\n3 ] Type 3 to Create ==> Tied Entry\n=>")
 		dup_record()
 		print("\nRecord added Successfully.......\n")
 		menu()
@@ -177,7 +178,7 @@ def dup_record():
 
 
 def menu():
-	Z=int(input("\nMain menu: \n1 ] Type 1 to open ==> GAME\n2 ] Type 2 to open ==> RECORDS\n3 ] Type 3 to open ==> CLEAR RECORDS\n4 ] Type 4 to open ==> Duplicate RECORDS\n=>"))
+	Z=int(input("\nMain menu: \n1 ] Type 1 to open ==> GAME\n2 ] Type 2 to open ==> RECORDS\n3 ] Type 3 to open ==> CLEAR RECORDS\n4 ] Type 4 to open ==> Duplicate RECORDS\n5 ] Type 5 to exit ==> GAME\n\n=>"))
 	
 	if (Z==1):
 		game()
@@ -187,8 +188,10 @@ def menu():
 		clear_records()
 	if(Z==4):
 		menu_dup_record()
+	if(Z==5):
+		sys.exit("Game Exited !!")
 	else:
-		print("/nPlz make valid choise...")
+		print("/nPlease make valid choice...")
 		menu()
 
 	
